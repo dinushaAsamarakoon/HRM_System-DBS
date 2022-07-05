600beea7b5ec5ff7bf21bdd8c1e6cca4068b3884
HarshanaEranda <harshawije1998@gmail.com>
2022-07-05 13:42:56 +0530

    link bootsrap

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -8,7 +8,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!--    CSS only-->
-    <link href="../../content/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!--    <link href="../../content/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../../content/custom.css">
     <!--Javascript only-->
     <script src="../../content/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
@@ -21,7 +22,7 @@
 <div class="shadow p-3 mb-5  rounded" >
 
 <!--    ============================================-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-end">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -29,8 +30,8 @@
 
         <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
 
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
-                <li class="nav-item">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 justify-content-end">
+                <li class="nav-item ">
                     <a class="nav-link " href="../register/login.php" style="color: #0c63e4;font-size: 20px;">Sign In</a>
                 </li>
 
