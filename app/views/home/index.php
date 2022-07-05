9f2a0932543b6782ea77f4efc0f73ebfbb5e9b25
HarshanaEranda <harshawije1998@gmail.com>
2022-07-05 18:57:38 +0530

    changed some style

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -16,21 +16,22 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <title>Home Page</title>
 </head>
 <body >
 <div class="shadow p-3 mb-5  rounded" >
 
 <!--    ============================================-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-end">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
+        <div class="collapse nav-tabs navbar-collapse justify-content-end " id="navbarTogglerDemo03">
 
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 justify-content-end">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
                 <li class="nav-item ">
                     <a class="nav-link " href="<?=SROOT?>EmployeeRegister/login" style="color: #0c63e4;font-size: 20px;">Sign In</a>
                 </li>
--- app/views/register/login.php
+++ app/views/register/login.php
@@ -8,13 +8,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!--    CSS only-->
 <!--    <link href="../../content/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../../content/custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
     <!--Javascript only-->
     <script src="../../content/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <title>Login Page</title>
 </head>
 <body >
@@ -26,9 +28,9 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse justify-content-end " id="navbarScroll" >
+            <div class="collapse  navbar-collapse justify-content-end " id="navbarScroll" >
 
-                <ul class="nav nav-link  " id="pills-tab" role="tablist">
+                <ul class="nav nav-tabs   " id="pills-tab" role="tablist">
                     <li class="nav-item">
                            <a class="nav-link active " id="pills-home-tab"  aria-current="page" href="<?=SROOT?>" style="color: #0c63e4;font-size: 20px;">Home</a>
                     </li>
@@ -48,13 +50,13 @@
 
         <div class="tab-content" id="pills-tabContent2">
             <!--This is the login page of the website        -->
-            <div  id="pills-login" role="tabpanel" aria-labelledby="pills-profile-tab" >
-                <div class="d-flex justify-content-center w-100  p-3"  >
-                    <div class="login w-25  p-3 shadow-lg p-3 mb-5 rounded h-100 d-inline-block">
-                        <form action="<?=SROOT?>EmployeeRegister/login" method="POST">
-                            <div class="mb-3 ">
+            <div class="h-100 " id="pills-login" role="tabpanel" aria-labelledby="pills-profile-tab" >
+                <div class="d-flex justify-content-center w-100 h-100  p-3"  >
+                    <div class=" w-50  p-3 shadow-lg p-5  mb-5   " style="background-color:  #ecf0f9; " >
+                        <form action="<?=SROOT?>EmployeeRegister/login" method="POST" style="height: 450px;margin-top: 5px;">
+                            <div class="d-lg-flex justify-content-center m-1">
 
-                                <h3>Login</h3>
+                                <h3  >Login</h3>
 
                             </div>
                             <div class="mb-3 ">
@@ -70,7 +72,7 @@
                             </div>
                             <div class="mb-3 ">
 
-                                <a href="" ><label class="form-label"  style="fon"> <u>Forget password</u> </label></a>
+                                <a href="" ><label class="form-label"  > <u>Forget password</u> </label></a>
                             </div>
                             <div class="col text-center"><button type="submit" class="btn btn-primary" >Submit</button></div>
                         </form>
